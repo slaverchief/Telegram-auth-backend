e09a85d1dba05b40f83e41c4b43d70e9a4bdf249
Maga <uvaysov.2005@mail.ru>
2024-12-28 11:36:01 +0300

    PostgreSQL connect

--- DjangoProj/settings.py
+++ DjangoProj/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'DjangoProj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
